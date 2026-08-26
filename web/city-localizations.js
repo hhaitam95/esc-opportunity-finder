556c61d1f700dcd37b33f78b1020dd7312289a7b
hhaitam95 <haitam.hamdan95@gmail.com>
2026-08-26 21:41:55 +0100

    fix: fully normalize and localize city display

--- web/city-localizations.js
+++ web/city-localizations.js
@@ -3,32 +3,187 @@ const CITY_LOCALIZATION_OVERRIDES = {
   "IT|PALERMO": { fr: "Palerme", ar: "باليرمو" },
   "IT|ROME": { fr: "Rome", ar: "روما" },
   "IT|GENOA": { fr: "Gênes", ar: "جنوة" },
-  "IT|MILAN": { fr: "Milan", ar: "ميلانو" },
+  "IT|GENOVA": { fr: "Gênes", ar: "جنوة" },
+  "IT|MILAN": { fr: "Milan", ar: "ميلanو" },
+  "IT|MILANO": { fr: "Milan", ar: "ميلانو" },
   "IT|NAPLES": { fr: "Naples", ar: "نابولي" },
+  "IT|NAPOLI": { fr: "Naples", ar: "نابولي" },
   "IT|FLORENCE": { fr: "Florence", ar: "فلورنسا" },
+  "IT|FIRENZE": { fr: "Florence", ar: "فلورنسا" },
+  "IT|BOLOGNA": { fr: "Bologne", ar: "بولونيا" },
+  "IT|PADOVA": { fr: "Padoue", ar: "بادوفا" },
+  "IT|VERONA": { fr: "Vérone", ar: "فيرونا" },
+  "IT|VITERBO": { fr: "Viterbe", ar: "فيتربو" },
+  "IT|MESSINA": { fr: "Messine", ar: "ميسينا" },
+  "IT|MATERA": { fr: "Matera", ar: "ماتيرا" },
+  "IT|BARI": { fr: "Bari", ar: "باري" },
+  "IT|PARMA": { fr: "Parme", ar: "بارما" },
+  "IT|RAVENNA": { fr: "Ravenne", ar: "رافينا" },
+  "IT|PESARO": { fr: "Pesaro", ar: "بيزارو" },
+  "IT|VICENZA": { fr: "Vicence", ar: "فيتشنزا" },
+  "IT|COMO": { fr: "Côme", ar: "كومو" },
+  "IT|FRATTAMAGGIORE": { fr: "Frattamaggiore", ar: "فراتاماجيوري" },
+
+  "FR|PARIS": { fr: "Paris", ar: "باريس" },
+  "FR|LYON": { fr: "Lyon", ar: "ليون" },
+  "FR|MARSEILLE": { fr: "Marseille", ar: "مرسيليا" },
+  "FR|TOULOUSE": { fr: "Toulouse", ar: "تولوز" },
+  "FR|NANTES": { fr: "Nantes", ar: "نانت" },
+  "FR|BORDEAUX": { fr: "Bordeaux", ar: "بوردو" },
+  "FR|LILLE": { fr: "Lille", ar: "ليل" },
+  "FR|STRASBOURG": { fr: "Strasbourg", ar: "ستراسبورغ" },
+  "FR|MONTPELLIER": { fr: "Montpellier", ar: "مونبلييه" },
+  "FR|GRENOBLE": { fr: "Grenoble", ar: "غرونوبل" },
+  "FR|REIMS": { fr: "Reims", ar: "ريمس" },
+  "FR|RENNES": { fr: "Rennes", ar: "رون" },
+  "FR|CAEN": { fr: "Caen", ar: "كان" },
+  "FR|TOURS": { fr: "Tours", ar: "تور" },
+  "FR|PAU": { fr: "Pau", ar: "بو" },
+  "FR|METZ": { fr: "Metz", ar: "ميتز" },
+  "FR|NANCY": { fr: "Nancy", ar: "نانسي" },
+  "FR|MULHOUSE": { fr: "Mulhouse", ar: "ميلوز" },
+  "FR|ANGERS": { fr: "Angers", ar: "أنجيه" },
+  "FR|LA ROCHELLE": { fr: "La Rochelle", ar: "لاروشيل" },
+  "FR|CLERMONT-FERRAND": { fr: "Clermont-Ferrand", ar: "كليرمون-فيران" },
+  "FR|FORT DE FRANCE": { fr: "Fort-de-France", ar: "فور دو فرانس" },
+
+  "DE|BERLIN": { fr: "Berlin", ar: "برلين" },
   "DE|MUNICH": { fr: "Munich", ar: "ميونخ" },
+  "DE|MÜNCHEN": { fr: "Munich", ar: "ميونخ" },
   "DE|COLOGNE": { fr: "Cologne", ar: "كولونيا" },
+  "DE|KOLN": { fr: "Cologne", ar: "كولونيا" },
+  "DE|KÖLN": { fr: "Cologne", ar: "كولونيا" },
+  "DE|FRANKFURT (ODER)": { fr: "Francfort-sur-l'Oder", ar: "فرانكفورت أودر" },
+  "DE|FRANKFURT ODER": { fr: "Francfort-sur-l'Oder", ar: "فرانكفورت أودر" },
+  "DE|WUERZBURG": { fr: "Würzburg", ar: "فورتسبورغ" },
+  "DE|SAARBRÜCKEN": { fr: "Sarrebruck", ar: "ساربروكن" },
+  "DE|Saarbrücken": { fr: "Sarrebruck", ar: "ساربروكن" },
+  "DE|GÖTTINGEN": { fr: "Göttingen", ar: "غوتينغن" },
+
+  "ES|MADRID": { fr: "Madrid", ar: "مدريد" },
   "ES|BARCELONA": { fr: "Barcelone", ar: "برشلونة" },
   "ES|SEVILLE": { fr: "Séville", ar: "إشبيلية" },
+  "ES|SEVILLA": { fr: "Séville", ar: "إشبيلية" },
+  "ES|BILBAO": { fr: "Bilbao", ar: "بلباو" },
+  "ES|VALENCIA": { fr: "Valence", ar: "فالنسيا" },
+  "ES|ZARAGOZA": { fr: "Saragosse", ar: "سرقسطة" },
+  "ES|ALICANTE": { fr: "Alicante", ar: "أليكانتي" },
+  "ES|MALAGA": { fr: "Malaga", ar: "مالقة" },
+  "ES|MALAGÓN": { fr: "Malagón", ar: "مالاغون" },
+  "ES|GIJON": { fr: "Gijón", ar: "خيخون" },
+  "ES|A CORUÑA": { fr: "La Corogne", ar: "لا كورونيا" },
+  "ES|OURANSE": { fr: "Ourense", ar: "أورينسي" },
+  "ES|SANTIAGO DE COMPOSTELA": { fr: "Saint-Jacques-de-Compostelle", ar: "سانتياغو دي كومبوستيلا" },
+  "ES|PALMA DE MALLORCA": { fr: "Palma de Majorque", ar: "بالما دي مايوركا" },
+  "ES|JEREZ DE LA FRONTERA": { fr: "Jerez de la Frontera", ar: "خيريز دي لا فرونتيرا" },
+
   "PT|LISBON": { fr: "Lisbonne", ar: "لشبونة" },
+  "PT|LISBOA": { fr: "Lisbonne", ar: "لشبونة" },
+  "PT|PORTO": { fr: "Porto", ar: "بورتو" },
+  "PT|FUNCHAL": { fr: "Funchal", ar: "فونشال" },
+  "PT|BRAGA": { fr: "Braga", ar: "براغا" },
+  "PT|AVEIRO": { fr: "Aveiro", ar: "أفيرو" },
+  "PT|ÉVORA": { fr: "Évora", ar: "إيفورا" },
+  "PT|EVORA": { fr: "Évora", ar: "إيفورا" },
+  "PT|PORTIMÃO": { fr: "Portimão", ar: "بورتيماو" },
+
+  "NL|AMSTERDAM": { fr: "Amsterdam", ar: "أمستردام" },
+  "NL|'S-HERTOGENBOSCH": { fr: "Bois-le-Duc", ar: "س هيرتوخينبوش" },
   "BE|BRUSSELS": { fr: "Bruxelles", ar: "بروكسل" },
+  "BE|BRUXELLES": { fr: "Bruxelles", ar: "بروكسل" },
+  "BE|BRUSSEL": { fr: "Bruxelles", ar: "بروكسل" },
+  "BE|ANTWERPEN": { fr: "Anvers", ar: "أنتويرب" },
+  "BE|TOURNAI": { fr: "Tournai", ar: "تورن" },
   "GR|ATHENS": { fr: "Athènes", ar: "أثينا" },
+  "GR|ATHINA": { fr: "Athènes", ar: "أثينا" },
+  "EL|ATHENS": { fr: "Athènes", ar: "أثينا" },
+  "EL|ATHINA": { fr: "Athènes", ar: "أثينا" },
+  "EL|THESSALONIKI": { fr: "Thessalonique", ar: "سالونيك" },
+  "EL|HERAKLION, KRETA": { fr: "Héraklion, Crète", ar: "هيراكليون، كريت" },
+  "CY|NICOSIA": { fr: "Nicosie", ar: "نيقوسيا" },
+  "CY|PAPHOS": { fr: "Paphos", ar: "بافوس" },
+  "CY|PAFOS": { fr: "Paphos", ar: "بافوس" },
   "TR|ISTANBUL": { fr: "Istanbul", ar: "إسطنبول" },
   "TR|ANKARA": { fr: "Ankara", ar: "أنقرة" },
+  "TR|NİĞDE": { fr: "Niğde", ar: "نيغدة" },
+  "TR|SİVAS": { fr: "Sivas", ar: "سيواس" },
+  "TR|SANLIURFA": { fr: "Şanlıurfa", ar: "شانلي أورفا" },
+  "TR|ŞANLIURFA": { fr: "Şanlıurfa", ar: "شانلي أورفا" },
+  "TR|GAZİANTEP": { fr: "Gaziantep", ar: "غازي عنتاب" },
+  "TR|GAZIANTEP": { fr: "Gaziantep", ar: "غازي عنتاب" },
+  "TR|DENIZLI": { fr: "Denizli", ar: "دنيزلي" },
+  "TR|ADANA": { fr: "Adana", ar: "أضنة" },
+  "TR|MUGLA": { fr: "Muğla", ar: "موغلا" },
+  "TR|ÇANKIRI": { fr: "Çankırı", ar: "جانقيري" },
+  "TR|ÇANKAYA - ANKARA": { fr: "Çankaya, Ankara", ar: "تشانكايا، أنقرة" },
+  "TR|ÇANKAYA / ANKARA": { fr: "Çankaya, Ankara", ar: "تشانكايا، أنقرة" },
+  "TR|ÇANKAYA/ANKARA": { fr: "Çankaya, Ankara", ar: "تشانكايا، أنقرة" },
+  "TR|KEPEZ / ANTALYA": { fr: "Kepez, Antalya", ar: "كيبيز، أنطاليا" },
+  "TR|YENİMAHALLE": { fr: "Yenimahalle", ar: "يني محلة" },
+  "TR|YENİMAHALLE / ANKARA": { fr: "Yenimahalle, Ankara", ar: "يني محلة، أنقرة" },
+
   "PL|WARSAW": { fr: "Varsovie", ar: "وارسو" },
+  "PL|WARSZAWA": { fr: "Varsovie", ar: "وارسو" },
+  "PL|WARSAW / WARSZAWA": { fr: "Varsovie", ar: "وارسو" },
   "PL|KRAKOW": { fr: "Cracovie", ar: "كراكوف" },
+  "PL|KRAKÓW": { fr: "Cracovie", ar: "كراكوف" },
+  "PL|CRACOVIA": { fr: "Cracovie", ar: "كراكوف" },
+  "PL|GDANSK": { fr: "Gdańsk", ar: "غدانسك" },
+  "PL|GDAŃSK": { fr: "Gdańsk", ar: "غدانسك" },
+  "PL|WROCLAW": { fr: "Wrocław", ar: "فروتسواف" },
+  "PL|WROCŁAW": { fr: "Wrocław", ar: "فروتسواف" },
+  "PL|BIALYSTOK": { fr: "Białystok", ar: "بياليستوك" },
+  "PL|BIAŁYSTOK": { fr: "Białystok", ar: "بياليستوك" },
+  "PL|JELENIA GORA": { fr: "Jelenia Góra", ar: "يلينيا غورا" },
+  "PL|JELENIA GÓRA": { fr: "Jelenia Góra", ar: "يلينيا غورا" },
+  "PL|POZNAN": { fr: "Poznań", ar: "بوزنان" },
+  "PL|ŁÓDŹ": { fr: "Łódź", ar: "لودز" },
+  "PL|STETTINO": { fr: "Szczecin", ar: "شتشيتسين" },
+
   "CZ|PRAGUE": { fr: "Prague", ar: "براغ" },
+  "CZ|PRAHA": { fr: "Prague", ar: "براغ" },
+  "CZ|NÁCHOD": { fr: "Náchod", ar: "ناخود" },
   "CZ|NACHOD": { fr: "Náchod", ar: "ناخود" },
+  "CZ|BRNO": { fr: "Brno", ar: "برنو" },
+  "CZ|PLZEŇ": { fr: "Plzeň", ar: "بلزن" },
+  "CZ|USTI NAD LABEM": { fr: "Ústí nad Labem", ar: "أوستي ناد لابم" },
+  "CZ|Usti nad Labem": { fr: "Ústí nad Labem", ar: "أوستي ناد لابم" },
+  "CZ|OSTRAVA - NOVÁ VES": { fr: "Ostrava - Nová Ves", ar: "أوسترافا - نوفا فيس" },
+  "CZ|ČESKÝ TĚŠÍN": { fr: "Český Těšín", ar: "تشييسكي تيشين" },
+  "CZ|HUSTOPEČE": { fr: "Hustopeče", ar: "هوستوبتشه" },
+  "CZ|KROMĚŘÍŽ": { fr: "Kroměříž", ar: "كروميريژ" },
+  "CZ|SPÁLENÉ POŘÍČÍ": { fr: "Spálené Poříčí", ar: "سباليني بوريتشي" },
+  "CZ|VELKÁ BÍTEŠ": { fr: "Velká Bíteš", ar: "فيلكا بيتش" },
+  "CZ|VSETÍN": { fr: "Vsetín", ar: "فيستين" },
+
   "AT|VIENNA": { fr: "Vienne", ar: "فيينا" },
+  "AT|WIEN": { fr: "Vienne", ar: "فيينا" },
   "CH|GENEVA": { fr: "Genève", ar: "جنيف" },
+  "CH|GENÈVE": { fr: "Genève", ar: "جنيف" },
+  "CH|ZURICH": { fr: "Zurich", ar: "زيورخ" },
+  "CH|ZÜRICH": { fr: "Zurich", ar: "زيورخ" },
   "RO|BUCHAREST": { fr: "Bucarest", ar: "بوخارست" },
-  "GB|LONDON": { fr: "Londres", ar: "لندن" },
-  "GB|EDINBURGH": { fr: "Édimbourg", ar: "إدنبرة" },
-  "IE|DUBLIN": { fr: "Dublin", ar: "دبلن" },
+  "RO|BUCURESTI": { fr: "Bucarest", ar: "بوخارست" },
+  "RO|BUCUREȘTI": { fr: "Bucarest", ar: "بوخارست" },
+  "RO|TIMISOARA": { fr: "Timișoara", ar: "تيميشوارا" },
+  "RO|TIMIȘOARA": { fr: "Timișoara", ar: "تيميشوارا" },
+  "RO|IASI": { fr: "Iași", ar: "ياش" },
+  "RO|IAȘI": { fr: "Iași", ar: "ياش" },
+  "RO|CLUJ-NAPOCA": { fr: "Cluj-Napoca", ar: "كلوج نابوكا" },
+
+  "HU|BUDAPEST": { fr: "Budapest", ar: "بودابست" },
+  "HU|DEBRECEN": { fr: "Debrecen", ar: "ديبريتسين" },
+  "HU|PÉCS": { fr: "Pécs", ar: "بيتش" },
   "HR|ZAGREB": { fr: "Zagreb", ar: "زغرب" },
+  "HR|SPLIT": { fr: "Split", ar: "سبليت" },
   "SI|LJUBLJANA": { fr: "Ljubljana", ar: "ليوبليانا" },
   "SK|BRATISLAVA": { fr: "Bratislava", ar: "براتيسلافا" },
+  "SK|BANSKA BYSTRICA": { fr: "Banská Bystrica", ar: "بانسكا بيستريتسا" },
+  "SK|BANSKÁ BYSTRICA": { fr: "Banská Bystrica", ar: "بانسكا بيستريتسا" },
+  "SK|KOŠICE": { fr: "Košice", ar: "كوشيتسه" },
   "BG|SOFIA": { fr: "Sofia", ar: "صوفيا" },
+  "BG|BURGAS": { fr: "Bourgas", ar: "بورغاس" },
+  "BG|VARNA": { fr: "Varna", ar: "فارنا" },
   "RS|BELGRADE": { fr: "Belgrade", ar: "بلغراد" },
   "BA|SARAJEVO": { fr: "Sarajevo", ar: "سراييفو" },
   "AL|TIRANA": { fr: "Tirana", ar: "تيرانا" },
@@ -36,8 +191,48 @@ const CITY_LOCALIZATION_OVERRIDES = {
   "LT|VILNIUS": { fr: "Vilnius", ar: "فيلنيوس" },
   "LV|RIGA": { fr: "Riga", ar: "ريغا" },
   "EE|TALLINN": { fr: "Tallinn", ar: "تالين" },
-  "CY|NICOSIA": { fr: "Nicosie", ar: "نيقوسيا" },
+  "EE|TALLIN": { fr: "Tallinn", ar: "تالين" },
   "MT|VALLETTA": { fr: "La Valette", ar: "فاليتا" },
+  "IE|DUBLIN": { fr: "Dublin", ar: "دبلن" },
+  "GB|LONDON": { fr: "Londres", ar: "لندن" },
+  "GB|EDINBURGH": { fr: "Édimbourg", ar: "إدنبرة" },
+  "SE|STOCKHOLM": { fr: "Stockholm", ar: "ستوكهولم" },
+  "DK|COPENHAGEN": { fr: "Copenhague", ar: "كوبنهاغن" },
+  "DK|KOBENHAVN": { fr: "Copenhague", ar: "كوبنهاغن" },
+  "NO|OSLO": { fr: "Oslo", ar: "أوسلو" },
+  "FI|HELSINKI": { fr: "Helsinki", ar: "هلسنكي" },
+  "IS|REYKJAVIK": { fr: "Reykjavík", ar: "ريكيافيك" },
+  "IS|REYKJAVÍK": { fr: "Reykjavík", ar: "ريكيافيك" },
+  "LU|LUXEMBOURG": { fr: "Luxembourg", ar: "لوكسمبورغ" },
+  "MA|GUELMIN": { fr: "Guelmim", ar: "كلميم" },
+  "MA|JERADA": { fr: "Jerada", ar: "جرادة" },
+  "MA|LARACHE": { fr: "Larache", ar: "العرائش" },
+  "MA|MEKNES": { fr: "Meknès", ar: "مكناس" },
+  "TN|MEDENINE": { fr: "Médenine", ar: "مدنين" },
+  "TN|MÉDENINE": { fr: "Médenine", ar: "مدنين" },
+  "TN|NABEUL": { fr: "Nabeul", ar: "نابل" },
+  "TN|SFAX": { fr: "Sfax", ar: "صفاقس" },
+  "TN|SOUSSE": { fr: "Sousse", ar: "سوسة" },
+  "SN|DAKAR": { fr: "Dakar", ar: "داكار" },
+  "SN|THIES": { fr: "Thiès", ar: "تييس" },
+  "TG|LOMÉ": { fr: "Lomé", ar: "لومي" },
+  "EG|CAIRO": { fr: "Le Caire", ar: "القاهرة" },
+  "EG|ALEXANDRIE": { fr: "Alexandrie", ar: "الإسكندرية" },
+  "EG|SIWA": { fr: "Siwa", ar: "سيوة" },
+  "JO|AMMAN": { fr: "Amman", ar: "عمّان" },
+  "NP|KATHMANDU": { fr: "Katmandou", ar: "كاتماندو" },
+  "PE|LIMA": { fr: "Lima", ar: "ليما" },
+  "EC|QUITO": { fr: "Quito", ar: "كيتو" },
+  "CO|AGUACHICA": { fr: "Aguachica", ar: "أغواتشيكا" },
+  "CG|BRAZZAVILLE": { fr: "Brazzaville", ar: "برازافيل" },
+  "CM|YAOUNDÉ": { fr: "Yaoundé", ar: "ياوندي" },
+  "GH|TAMALE": { fr: "Tamale", ar: "تامالي" },
+  "GT|CIUDAD DE GUATEMALA": { fr: "Guatemala", ar: "مدينة غواتيمالا" },
+  "HN|TEGUCIGALPA": { fr: "Tegucigalpa", ar: "تيغوسيغالبا" },
+  "KE|NAIROBI": { fr: "Nairobi", ar: "نيروبي" },
+  "UG|KAMPALA": { fr: "Kampala", ar: "كمبالا" },
+  "VN|HANOI": { fr: "Hanoï", ar: "هانوي" },
+  "UA|VINNYTSIA": { fr: "Vinnytsia", ar: "فينيتسا" },
 };
 
 function normalizeCityKey(country, city) {
@@ -52,12 +247,28 @@ function normalizeCityKey(country, city) {
   return `${normalizedCountry}|${normalizedCity}`;
 }
 
+function languageFor(locale) {
+  const value = String(locale || "en-GB");
+  if (value.startsWith("fr")) return "fr";
+  if (value.startsWith("ar")) return "ar";
+  return "en";
+}
+
+function titleCaseLocation(value) {
+  return String(value || "")
+    .trim()
+    .toLocaleLowerCase()
+    .replace(/(^|[\s\-/'’(])([\p{L}])/gu, (_, prefix, letter) => `${prefix}${letter.toLocaleUpperCase()}`)
+    .replace(/\s+/g, " ")
+    .trim();
+}
+
 const ARABIC_DIGRAPHS = [
   ["sch", "ش"],
   ["tch", "تش"],
   ["shr", "شر"],
+  ["ch", "تش"],
   ["sh", "ش"],
-  ["ch", "خ"],
   ["th", "ث"],
   ["dh", "ذ"],
   ["kh", "خ"],
@@ -70,6 +281,7 @@ const ARABIC_DIGRAPHS = [
   ["ee", "ي"],
   ["ie", "ي"],
   ["ei", "ي"],
+  ["tion", "تيون"],
 ];
 
 const ARABIC_LETTERS = {
@@ -101,25 +313,33 @@ const ARABIC_LETTERS = {
   z: "ز",
 };
 
-function arabicTransliteration(city) {
-  let value = String(city || "")
+function arabicTransliteration(value) {
+  let source = String(value || "").trim().toLocaleLowerCase();
+  if (!source) return "";
+
+  source = source
     .normalize("NFD")
     .replace(/[\u0300-\u036f]/g, "")
-    .trim()
-    .toLocaleLowerCase();
-  if (!value) return "";
+    .replace(/ı/g, "i")
+    .replace(/ñ/g, "n")
+    .replace(/ç/g, "c")
+    .replace(/ß/g, "ss")
+    .replace(/æ/g, "ae")
+    .replace(/œ/g, "oe");
 
   for (const [from, to] of ARABIC_DIGRAPHS) {
-    value = value.replaceAll(from, to);
+    source = source.replaceAll(from, to);
   }
 
   let output = "";
-  for (const char of value) {
+  for (const char of source) {
     if (/\p{Script=Arabic}/u.test(char)) {
       output += char;
-      continue;
+    } else if (/[a-z]/.test(char)) {
+      output += ARABIC_LETTERS[char];
+    } else {
+      output += char;
     }
-    output += ARABIC_LETTERS[char] || char;
   }
 
   return output
@@ -129,25 +349,15 @@ function arabicTransliteration(city) {
 }
 
 export function localizeNewCity(country, city, locale) {
-  const language = String(locale || "en-GB").startsWith("fr")
-    ? "fr"
-    : String(locale || "en-GB").startsWith("ar")
-      ? "ar"
-      : "en";
-
+  const language = languageFor(locale);
   const original = String(city || "").trim();
-  if (language === "en" || !original) return original;
+  if (!original || language === "en") return titleCaseLocation(original);
 
   const key = normalizeCityKey(country, original);
   const override = CITY_LOCALIZATION_OVERRIDES[key];
   if (override?.[language]) return override[language];
 
-  // French city names are often identical to the local/international name.
-  // Do not invent French translations for obscure towns.
-  if (language === "fr") return original;
-
-  // For previously unseen Arabic locations, use a deterministic transliteration
-  // rather than a network lookup. Explicit overrides remain authoritative.
+  if (language === "fr") return titleCaseLocation(original);
   return arabicTransliteration(original) || original;
 }
 
